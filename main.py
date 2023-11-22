ca5f02290f0f5c41e3a69991a40a46b93463e897
Gakuto Furuya <g.furuya@gaato.net>
2023-11-22 23:52:59 +0900

    v2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
 import os
 import time
+from typing import Any
 
 import discord
 from discord.ext import commands, tasks
@@ -13,6 +15,31 @@ admin_only = discord.Permissions()
 admin_only.administrator = True
 
 bot = commands.Bot(intents=intents)
+
+
+class FileDict(dict):
+    def __init__(self, path: str) -> None:
+        self.path = path
+        if not os.path.exists(path):
+            with open(path, "w") as f:
+                json.dump({}, f)
+        with open(path, "r") as f:
+            self.update(json.load(f))
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        super().__setitem__(key, value)
+        with open(self.path, "w") as f:
+            json.dump(self, f)
+
+    def __delitem__(self, key: str) -> None:
+        super().__delitem__(key)
+        with open(self.path, "w") as f:
+            json.dump(self, f)
+
+
+main_to_sub = FileDict("data/main-to-sub.json")
+sub_to_main = FileDict("data/sub-to-main.json")
+punishment = FileDict("data/punishment.json")
 
 
 async def remove_manage_roles(member: discord.Member) -> None:
@@ -37,44 +64,92 @@ async def on_ready() -> None:
 
 @bot.event
 async def on_member_update(before: discord.Member, after: discord.Member) -> None:
-    admin_role = after.guild.get_role(int(os.environ["ADMIN_ROLE_ID"]))
-    for role in after.roles:
-        if role.name == "罰" and role > admin_role:
-            if int(time.time()) // (24 * 60 * 60) - role.color.value >= 30:
-                await role.delete()
-            else:
-                await remove_manage_roles(after)
+    if str(after.id) in sub_to_main:
+        main = after.guild.get_member(sub_to_main[str(after.id)])
+    else:
+        main = after
+    if str(main.id) in punishment:
+        if punishment[str(main.id)] > time.time():
+            await remove_manage_roles(after)
+        else:
+            del punishment[str(main.id)]
 
 
-@bot.user_command(name="Punish", default_member_permissions=admin_only)
-async def punish(ctx: discord.ApplicationContext, member: discord.Member) -> None:
-    admin_role = ctx.guild.get_role(int(os.environ["ADMIN_ROLE_ID"]))
-    await remove_manage_roles(member)
-    role = await ctx.guild.create_role(
-        name="罰", color=discord.Color(int(time.time()) // (24 * 60 * 60))
+@bot.slash_command(
+    name="link-user",
+    default_member_permissions=admin_only,
+    guild_ids=[int(os.environ["GUILD_ID"])],
+)
+async def link_user(
+    ctx: discord.ApplicationContext, main: discord.Member, sub: discord.Member
+) -> None:
+    main_to_sub[str(main.id)] = main_to_sub.get(str(main.id), []) + [str(sub.id)]
+    sub_to_main[str(sub.id)] = main.id
+    await ctx.respond(f"{main.mention} is now linked to {sub.mention}!", ephemeral=True)
+
+
+@bot.slash_command(
+    name="unlink-user",
+    default_member_permissions=admin_only,
+    guild_ids=[int(os.environ["GUILD_ID"])],
+)
+async def unlink_user(
+    ctx: discord.ApplicationContext, main: discord.Member, sub: discord.Member
+) -> None:
+    main_to_sub[str(main.id)].remove(str(sub.id))
+    del sub_to_main[str(sub.id)]
+    await ctx.respond(
+        f"{main.mention} is now unlinked from {sub.mention}!", ephemeral=True
     )
-    await role.edit(position=admin_role.position + 1)
-    await member.add_roles(role)
-    await ctx.respond(f"{member.mention} has been punished!", ephemeral=True)
 
 
-@bot.user_command(name="Forgive", default_member_permissions=admin_only)
+@bot.user_command(
+    name="Punish",
+    default_member_permissions=admin_only,
+    guild_ids=[int(os.environ["GUILD_ID"])],
+)
+async def punish(ctx: discord.ApplicationContext, member: discord.Member) -> None:
+    await remove_manage_roles(member)
+    punishment[str(member.id)] = time.time() + 24 * 60 * 60 * 30
+    await ctx.respond(f"{member.mention} is now punished!", ephemeral=True)
+
+
+@bot.user_command(
+    name="Forgive",
+    default_member_permissions=admin_only,
+    guild_ids=[int(os.environ["GUILD_ID"])],
+)
 async def forgive(ctx: discord.ApplicationContext, member: discord.Member) -> None:
-    await member.remove_roles(discord.utils.get(ctx.guild.roles, name="罰"))
-    await ctx.respond(f"{member.mention} has been forgiven!", ephemeral=True)
+    del punishment[str(member.id)]
+    await ctx.respond(f"{member.mention} is now forgiven!", ephemeral=True)
+
+
+@bot.slash_command(
+    name="list-punishments",
+    guild_ids=[int(os.environ["GUILD_ID"])],
+)
+async def list_punishments(ctx: discord.ApplicationContext) -> None:
+    await ctx.respond(
+        "\n".join(
+            f"{bot.get_user(int(k)).mention} is punished until <t:{int(v)}:F>"
+            for k, v in punishment.items()
+        ),
+        ephemeral=True,
+    )
 
 
 @tasks.loop(hours=1)
 async def check() -> None:
-    guild = bot.get_guild(int(os.environ["GUILD_ID"]))
-    admin_role = guild.get_role(int(os.environ["ADMIN_ROLE_ID"]))
-    for member in guild.members:
-        for role in member.roles:
-            if role.name == "罰" and role > admin_role:
-                if int(time.time()) // (24 * 60 * 60) - role.color.value >= 30:
-                    await role.delete()
-                else:
-                    await remove_manage_roles(member)
+    for member in bot.get_guild(int(os.environ["GUILD_ID"])).members:
+        if str(member.id) in sub_to_main:
+            main = member.guild.get_member(sub_to_main[str(member.id)])
+        else:
+            main = member
+        if str(main.id) in punishment:
+            if punishment[str(main.id)] > time.time():
+                await remove_manage_roles(member)
+            else:
+                del punishment[str(main.id)]
 
 
 bot.run(os.environ["DISCORD_TOKEN"])
